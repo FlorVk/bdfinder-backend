583e264193ea3c0009db88d446346649742cc817
FlorVk <florvankuyk@hotmail.com>
2022-08-29 18:23:37 +0200

    Fix: try for fix jwt secret

--- passport/passport.js
+++ passport/passport.js
@@ -14,7 +14,7 @@ var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 var opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = config.get('jwt.secret');
+opts.secretOrKey = "Secret";
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findOne({
